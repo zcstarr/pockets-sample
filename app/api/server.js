f22a0864c4fdb187d189d5c0627b19679890f636
Zane Starr <zcstarr@Zanes-MacBook-Pro.local>
2018-01-18 22:02:28 -0800

    Refactor server api example

--- app/api/server.js
+++ app/api/server.js
@@ -1,42 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-const { PocketsHub, Pockets, Registry, Service, web3 } = require('../javascripts/contract');
+
+const {
+  PocketsHub,
+  Pockets,
+  Service,
+  web3
+} = require('../javascripts/contract');
+
 const yargs = require('yargs');
-const {argv} =  yargs;
 const app = express();
 const port = 3000;
 
-let accounts;
 let service;
 let hub;
 let owner;
-let registry;
 
-const args = yargs.option('verbose', {
-  alias: 'v',
-  default: false,
-  describe: 'Super verbose mode',
-})
-.option('hub', {
-  string: true,
-  demand: true,
-})
-.option('service', {
-  string: true,
-  demand: true,
-})
-.usage('Usage: $0 --hub [address] --service [address]')
-.argv
+const args = yargs
+  .option('hub', {
+    string: true,
+    demand: true,
+  })
+  .option('service', {
+    string: true,
+    demand: true,
+  })
+  .usage('Usage: $0 --hub [address] --service [address]')
+  .argv
 
-const users= {};
+const users = {};
 const tokenLookup = {};
 
 // Utility Functions 
 const getPocketDeadline = async (pocketAddress) => {
   const pocket = Pockets.at(pocketAddress)
   // balance, active, subscription, paymentAmount, paymentDeadline
-  let  [a,b ,c ,d , paymentDeadline] = await pocket.getPocket.call(service.address);
-  console.log([a, b, c, d, paymentDeadline]);
+  let [, , , , paymentDeadline] = await pocket.getPocket.call(service.address);
   return paymentDeadline;
 };
 
@@ -46,11 +45,13 @@ const checkDeadlineExpired = async (pocketAddress) => {
 }
 
 const collectPayment = async (pocketAddress) => {
-  console.log(`yep ${pocketAddress}`)
-  const tx = await service.requestHold(pocketAddress, { from: web3.eth.accounts[0] });
-  console.log(`yessir ${pocketAddress}`)
-  console.log(tx.logs[0].args);
-   return { gasUsed: tx.gasUsed, amountHeld: null };
+  const tx = await service.requestHold(pocketAddress, {
+    from: web3.eth.accounts[0]
+  });
+  return {
+    gasUsed: tx.gasUsed,
+    amountHeld: null
+  };
 };
 
 const getPocketBalance = async (pocketAddress) => {
@@ -66,7 +67,7 @@ app.use(cors());
 app.set('query parser', true);
 app.set('url parser', true);
 
-//Routes 
+// Routes 
 
 // Collect payments that are due from pockets
 app.post('/collect', async (request, response) => {
@@ -77,21 +78,22 @@ app.post('/collect', async (request, response) => {
     const [pocketAddress] = await service.getPocketByIdx.call(i);
     try {
       const expired = await checkDeadlineExpired(pocketAddress);
-      console.log(expired)
-      console.log(pocketAddress)
-      if (!expired){
-        console.log('Was not expired safe')
-        return
+      if (!expired) {
+        console.log('Was not expired, skipping collecting payment')
+        break;
       }
 
       console.log(`collecting payment from ${pocketAddress}`)
-      const { gasUsed } = await collectPayment(pocketAddress);
-      console.log(`used ${gasUsed}`);
+      const {
+        gasUsed
+      } = await collectPayment(pocketAddress);
+      console.log(`used ${gasUsed} collecting payment`);
       paymentCount++;
     } catch (e) {
       users[pocketAddress] = createNewToken();
       console.log('Accesss lost, withdraw from pocket failed')
       console.log(e);
+
     }
   }
   response.status(200);
@@ -101,24 +103,27 @@ app.post('/collect', async (request, response) => {
 //Grant an access token so users can play music
 app.post('/token/:pocketAddress', async (request, response) => {
 
-const {pocketAddress} = request.params;
-console.log(pocketAddress)
+  const {
+    pocketAddress
+  } = request.params;
+
   const expired = await checkDeadlineExpired(pocketAddress);
-  console.log(`noriega: ${expired}`)
-  console.log(expired);
   const newToken = createNewToken();
+
   if (!expired) {
     if (!users[pocketAddress])
       users[pocketAddress] = newToken;
     tokenLookup[users[pocketAddress]] = pocketAddress;
-    response.json({ token: users[pocketAddress] });
+    response.json({
+      token: users[pocketAddress]
+    });
     return;
   }
   try {
-    const { success } = await collectPayment(pocketAddress);
+    await collectPayment(pocketAddress);
     users[pocketAddress] = users[pocketAddress] ? users[pocketAddress] : newToken;
     tokenLookup[users[pocketAddress]] = pocketAddress;
-    response.json({token: users[pocketAddress]});
+    response.json({ token: users[pocketAddress] });
   } catch (e) {
     console.error(e);
     response.status(400);
@@ -134,7 +139,9 @@ app.post('/play', async (request, response) => {
 
   if (users[address] && users[address] === token) {
     const randomId = Math.random().toString(16).slice(2);
-    response.json({message: `Streaming track id:${randomId}`});
+    response.json({
+      message: `Streaming track id:${randomId}`
+    });
   }
   response.status(401);
   response.send();
@@ -142,13 +149,9 @@ app.post('/play', async (request, response) => {
 
 // Setup the pockethub address and service hub address
 app.listen(port, async (err) => {
-  console.log('starting to listen');
-    accounts = await web3.eth.getAccountsPromise();
-    debugger
-    console.log(args.service);
-    service = Service.at(args.service);
-    hub = PocketsHub.at(args.hub);
-    owner = await service.owner.call();
+  service = Service.at(args.service);
+  hub = PocketsHub.at(args.hub);
+  owner = await service.owner.call();
   if (err) {
     return console.log('something bad happened', err)
   }
@@ -157,4 +160,3 @@ app.listen(port, async (err) => {
   console.log('starting service with:');
   console.log(`hub:${hub.address}, service:${service.address}, owner:${owner}`);
 });
-
