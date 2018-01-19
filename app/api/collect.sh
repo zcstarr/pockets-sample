f44eb0a90bfb8cc672cbb20c8906717f452f22b9
Zane Starr <zcstarr@Zanes-MacBook-Pro.local>
2018-01-18 22:00:23 -0800

    Add additional output to bash script

--- app/api/collect.sh
+++ app/api/collect.sh
@@ -2,6 +2,7 @@
 echo "Press [CTRL+C] to stop.."
 while true
 do
+  echo 'collecting payment'
   collectionResult="$(curl -X POST http://localhost:3000/collect)" 
   echo $collectionResult
   sleep 30 
